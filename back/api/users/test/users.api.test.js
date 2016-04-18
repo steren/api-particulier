1c2720ce70f2de93cab301ca1048bf8f43eb5a82
Thibaut Géry <thibaut.gery@gmail.com>
2016-04-18 14:34:19 +0200

    can now get the profile

--- back/api/users/test/users.api.test.js
+++ back/api/users/test/users.api.test.js
@@ -1,18 +1,60 @@
 const expect = require('chai').expect;
 const request = require('request');
 const serverTest = require('./../../test/utils/server');
+const UsersService = require('./../users.service');
+const Redis = require('ioredis')
+const emptylogger = require('bunyan-blackhole');
+
 
 describe('User API', function () {
   const server = serverTest();
   const api = server.api;
 
-  describe("When getting the profile", () => {
-
-    it('replies with json and 200', (done) => {
-      api()
-        .get('/api/users/tge@octo.com')
-        .expect("content-type", /json/)
-        .expect(200,done)
+  describe('with Thibaut in DB', () => {
+    const redisHost = process.env['REDIS_PORT_HOST'] ||'127.0.0.1'
+    const redisDriver = new Redis(redisHost, 6379);
+    const usersService = new UsersService({
+      redis: {
+        driver: redisDriver,
+        userPrefix: 'user-test',
+        logger: emptylogger()
+      }
     });
-  });
+    const email = 'tge@octo.com'
+    const user = {
+      email,
+      name: 'gery',
+      surname: 'Thibaut',
+      role: 'user',
+      localAuthority: 'Paris',
+      keys: ['A', 'B']
+    }
+    beforeEach((done)  => {
+      usersService.setUser(user)
+      .then(() => done())
+      .catch((err) => done(err))
+    })
+
+    describe("When getting the profile", () => {
+      it('replies with json and 200', (done) => {
+        api()
+          .get('/api/users/' + email)
+          .expect("content-type", /json/)
+          .expect(200,user, done)
+      });
+    });
+
+    describe("When getting another profile", () => {
+      it('replies with json and 404', (done) => {
+        const error = {
+          "error": "not_found",
+          "reason": "L'utilisateur n'a pu être trouvé"
+        }
+        api()
+          .get('/api/users/notIn@gom.der')
+          .expect("content-type", /json/)
+          .expect(404, error, done)
+      });
+    });
+  })
 });
--- back/api/users/users.controller.js
+++ back/api/users/users.controller.js
@@ -1,9 +1,21 @@
+const UsersService = require('./users.service')
+const StandardError = require('standard-error');
+
 module.exports = UserController;
 
 function UserController(options) {
   options = options || {};
+  const usersService = new UsersService(options)
 
-  this.getProfile = function(req, res) {
-    res.json({})
+
+  this.getProfile = function(req, res, next) {
+    usersService.getUser(req.params.user_mail).then((user) => {
+      if(!user) {
+        return next(new StandardError('L\'utilisateur n\'a pu être trouvé', {code: 404}))
+      }
+      res.json(user);
+    }).catch((err) => {
+      next(new StandardError(err, {code: 500}));
+    })
   }
 }
